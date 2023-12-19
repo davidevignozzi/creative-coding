5bd2e113244b4435f09e3043320045937cba3c19
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-12-19 17:54:29 +0100

    :sparkles: Angles and rotation

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -4,6 +4,16 @@ const settings = {
   dimensions: [1080, 1080]
 };
 
+/**
+ * Converts degrees to radiants.
+ *
+ * @param {number} degrees - The angle in degrees to be converted.
+ * @returns {number} The equivalent angle in radiants.
+ */
+const degToRad = (degrees) => {
+  return (degrees / 180) * Math.PI;
+};
+
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
@@ -11,22 +21,19 @@ const sketch = () => {
 
     context.fillStyle = 'black';
 
-    context.save(); // <- save the state of the context
+    // const x = width * 0.5;
+    // const y = height * 0.5;
+    // const w = width * 0.3;
+    // const h = height * 0.3;
 
-    const x = width * 0.5;
-    const y = height * 0.5;
-    const w = width * 0.3;
-    const h = height * 0.3;
+    const cx = width * 0.5; // center of the circle
+    const cy = height * 0.5; // center of the circle
+    const w = width * 0.01;
+    const h = height * 0.1;
+    let x, y;
 
-    context.translate(x, y);
-    context.rotate(0.5);
-
-    context.beginPath();
-    // context.rect(x, y, w, h); // <- will be at the center left (without transform)
-    context.rect(-w * 0.5, -h * 0.5, w, h); // <- if translated (context.translate(x, y)) will be to the center
-    context.fill();
-
-    context.restore(); // <- restore the state of the context. In this way if i will create another figure, it will have the origin at [0, 0] (in the high left corner)
+    const num = 12; // <- number of copies
+    const radius = width * 0.3; // <- radius of the circle
 
     // * This will be the block:
     /**
@@ -36,6 +43,38 @@ const sketch = () => {
      *
      * context.restore();
      */
+
+    for (let i = 0; i < num; i++) {
+      const slice = degToRad(360 / num); // size of the slice
+      const angle = slice * i;
+
+      // x = cx + radius * Math.sin(angle);
+      // y = cy + radius * Math.cos(angle);
+      x = radius * Math.sin(angle);
+      y = radius * Math.cos(angle);
+
+      context.save(); // <- save the state of the context
+
+      /**
+       * Transformations
+       */
+      context.translate(cx, cy);
+      context.translate(x, y);
+
+      // to transform from degrees in radiant it deserve this formula {degrees / 180 * Math.PI}
+      // context.rotate(0.5); // is in gradiants not in degrees
+      // context.rotate(degToRad(45));
+
+      // context.rotate(angle);
+      context.rotate(-angle);
+
+      context.beginPath();
+      // context.rect(x, y, w, h); // <- will be at the center left (without transform)
+      context.rect(-w * 0.5, -h * 0.5, w, h); // <- if translated (context.translate(x, y)) will be to the center
+      context.fill();
+
+      context.restore(); // <- restore the state of the context. In this way if i will create another figure, it will have the origin at [0, 0] (in the high left corner)
+    }
   };
 };
 
