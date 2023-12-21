b6c9da4b7ac0a3a8e990c9b5b1f68eccf6fbeb15
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-12-21 11:26:57 +0100

    :dizzy: Animating agents

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -1,7 +1,8 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
 
-class Point {
+// ! renamed Point -> Vector
+class Vector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
@@ -9,24 +10,45 @@ class Point {
 }
 class Agent {
   constructor(x, y) {
-    this.pos = new Point(x, y);
-    this.radius = 10;
+    this.pos = new Vector(x, y); // ! renamed Point -> Vector
+    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
+    this.radius = random.range(4, 12);
   }
 
+  // Animations
+  bounce(width, height) {
+    if (this.pos.x <= 0 || this.pos.x >= width) this.vel.x *= -1;
+    if (this.pos.y <= 0 || this.pos.y >= height) this.vel.y *= -1;
+  }
+
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+  }
+
+  // Draw
   draw(context) {
-    context.fillStyle = 'black';
+    context.save();
+
+    context.translate(this.pos.x, this.pos.y);
 
     context.beginPath();
-    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
+
+    context.lineWidth = 4;
     context.fill();
+    context.stroke();
+
+    context.restore();
   }
 }
 
 const settings = {
-  dimensions: [1080, 1080]
+  dimensions: [1080, 1080],
+  animate: true
 };
 
-const sketch = ({ context, width, height }) => {
+const sketch = ({ width, height }) => {
   const agents = [];
 
   /**
@@ -67,7 +89,9 @@ const sketch = ({ context, width, height }) => {
     // agentB.draw(context);
 
     agents.forEach((agent) => {
+      agent.update();
       agent.draw(context);
+      agent.bounce(width, height);
     });
   };
 };
